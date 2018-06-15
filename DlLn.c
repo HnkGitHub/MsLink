7328ec1cb7eb1e91a5457d69ebb2caa2baaf9a65
HnkGitHub <hankhawkz@outlook.com>
2018-06-15 00:20:12 -0500

    Update DlLn.c

--- DlLn.c
+++ DlLn.c
@@ -1,15 +1,19 @@
+// DlLn.c
+// Delete Symbolic Links
+// Use Microsoft Visual C++
 #include <windows.h>
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
 	DWORD dwAttr ;
-
+	// Test for the presence of an argument
 	if (!argv[1])
 	{
 		printf ("Usage: dlln [Symbolic Link]\n") ;
 		return 0 ;
 	}
 
+	// Test File attributes to verify the file is a valid symbolic link before proceeding
 	dwAttr = GetFileAttributes(argv[1]) ;
 	if ((dwAttr & FILE_ATTRIBUTE_REPARSE_POINT) != FILE_ATTRIBUTE_REPARSE_POINT)
 	{
@@ -17,6 +21,9 @@ int main(int argc, char *argv[])
 		return 0 ;
 	}
 	else
+		
+	// Pseudo Logic:
+	// if link refers to a file, use DeleteFile, else use RemoveDirectory API.
 	{
 		if ((dwAttr & FILE_ATTRIBUTE_ARCHIVE) == FILE_ATTRIBUTE_ARCHIVE) 
 		{
