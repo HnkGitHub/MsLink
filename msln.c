ed8d51cdadd12063cd8e70afe326fa3fd0991062
HnkGitHub <hankhawkz@outlook.com>
2018-06-19 18:24:54 -0500

    Update msln.c
    Added additional code to provide informative error messages.

--- msln.c
+++ msln.c
@@ -1,3 +1,4 @@
+// msln.c
 #include <windows.h>
 #include <stdio.h>
 
@@ -5,9 +6,25 @@
 #define SYMBOLIC_LINK_FLAG_DIRECTORY 1
 #endif
 
+#ifndef SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE
+#define SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE 2
+#endif
+
+// Prototypes
+void PrintErrMsg (LPSTR) ;
+
 int main(int argc, char *argv[])
 {
 	DWORD dwAttr ;
+	
+	// Test for 2 arguments
+	if (argc != 3)
+	{
+		printf ("Usage: msln [symbolic link] [target]\n") ;
+		ExitProcess() ;
+	}
+	
+		
 	// Test for ARCHIVE or DIRECTORY Attribute
 	dwAttr = GetFileAttributes(argv[2]) ;
 	if ( (dwAttr & FILE_ATTRIBUTE_ARCHIVE) == FILE_ATTRIBUTE_ARCHIVE)
@@ -15,7 +32,11 @@ int main(int argc, char *argv[])
 		if (CreateSymbolicLinkA(argv[1], argv[2], SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE))
 		{
 			printf("Symbolic Link %s created as a file\n", argv[1]) ;
-		}	
+		}
+		else
+		{
+			PrintErrMsg ("Create SymLink File") ;
+		}
 	}
 	else
 	{
@@ -24,6 +45,32 @@ int main(int argc, char *argv[])
 			{
 				printf("Symbolic Link %s created as a Directory\n", argv[1]) ;
 			}
+			else
+			{
+				PrintErrMsg ("Create SymLink Directory") ;
+			}
 	}
 	return 0 ;
 }
+
+void PrintErrMsg (LPSTR lpszFunction) 
+{
+	LPVOID lpBuffer ;
+	DWORD dwError = GetLastError() ;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			dwError,
+			MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPSTR)&lpBuffer,
+			60,
+			NULL
+		     ) ;
+
+	printf("%s\n%sError: #%d\n", lpszFunction, (char *)lpBuffer, dwError) ;
+	LocalFree(lpBuffer) ;
+	ExitProcess(dwError) ;
+}
+
+
