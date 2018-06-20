aef99ac249c1d105372c9768649ecb8f0f3918e8
HnkGitHub <hankhawkz@outlook.com>
2018-06-20 14:02:41 -0500

    Update msln.c
    Added tests for Windows Versions and set flags for the different versions.

--- msln.c
+++ msln.c
@@ -1,6 +1,11 @@
 // msln.c
 #include <windows.h>
+#include <VersionHelpers.h>
 #include <stdio.h>
+
+#ifndef SYMBOLIC_LINK_FLAG_ARCHIVE
+#define SYMBOLIC_LINK_FLAG_ARCHIVE 0
+#endif
 
 #ifndef SYMBOLIC_LINK_FLAG_DIRECTORY
 #define SYMBOLIC_LINK_FLAG_DIRECTORY 1
@@ -16,7 +21,26 @@ void PrintErrMsg (LPSTR) ;
 int main(int argc, char *argv[])
 {
 	DWORD dwAttr ;
+	DWORD dwFlagFile, dwFlagDir ;
+	// Test for Windows Vista or Greater
+	if (!IsWindowsVistaOrGreater())
+	{
+		printf("Windows Version must be Vista or greater\n") ;
+		ExitProcess(0) ;
+	}
 	
+	// Set FLAGS for CreateSymbolicLink Function.
+	if (IsWindows10OrGreater())
+	{
+		dwFlagFile = SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE ;
+		dwFlagDir = SYMBOLIC_LINK_FLAG_DIRECTORY | SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE ;
+	}
+	else
+	{
+		dwFlagFile = SYMBOLIC_LINK_FLAG_ARCHIVE ;
+		dwFlagDir = SYMBOLIC_LINK_FLAG_DIRECTORY ;
+	}
+
 	// Test for 2 arguments
 	if (argc != 3)
 	{
@@ -29,7 +53,7 @@ int main(int argc, char *argv[])
 	dwAttr = GetFileAttributes(argv[2]) ;
 	if ( (dwAttr & FILE_ATTRIBUTE_ARCHIVE) == FILE_ATTRIBUTE_ARCHIVE)
 	{
-		if (CreateSymbolicLinkA(argv[1], argv[2], SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE))
+		if (CreateSymbolicLinkA(argv[1], argv[2], dwFlagFile))
 		{
 			printf("Symbolic Link %s created as a file\n", argv[1]) ;
 		}
@@ -41,7 +65,7 @@ int main(int argc, char *argv[])
 	else
 	{
 		if ( (dwAttr & FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY)
-			if (CreateSymbolicLinkA(argv[1], argv[2], SYMBOLIC_LINK_FLAG_DIRECTORY | SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE))
+			if (CreateSymbolicLinkA(argv[1], argv[2], dwFlagDir))
 			{
 				printf("Symbolic Link %s created as a Directory\n", argv[1]) ;
 			}
